93827a8cc47616818dd21db2689f28d034067fb8
Nikita <bigbasspoll@gmail.com>
2023-01-23 19:26:45 +0300

    creatingApp

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux'
+import { store } from '@/app/store'
+import '../styles/global.scss'
+import { ChakraProvider } from '@chakra-ui/provider'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+	return (
+		<Provider store={store}>
+			<ChakraProvider>
+				<Component {...pageProps} />
+			</ChakraProvider>
+		</Provider>
+	)
 }
+
+export default App
